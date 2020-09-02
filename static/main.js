92c94a3cfd56547be05c5b5306090cc59d1b3661
Ali Faraj <faraj7g@gmail.com>
2020-09-02 20:58:20 -0300

    Update main.js
    change API URL to Heroku

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@ import { searchRequest } from './search.js';
 
 //generate team and country list - once it is done, it will call the function to enable the search button
 setupForm();
-var searchstr = 'http://127.0.0.1:8000/projects/';
+var searchstr = 'https://igemapi.herokuapp.com/projects/';
 searchstr = searchstr + window.location.search + '&format=json';
 searchRequest(searchstr);
 
